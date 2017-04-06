6c6b52ba8ab3403167e05e02e667eda8204d957e
Davide Giovanelli <dgiovanelli@fbk.eu>
2017-04-06 16:15:45 +0200

    adding defines

--- scan.c
+++ scan.c
@@ -12,6 +12,24 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
+#define LE_LINK		0x80
+
+#define FLAGS_AD_TYPE 0x01
+#define FLAGS_LIMITED_MODE_BIT 0x01
+#define FLAGS_GENERAL_MODE_BIT 0x02
+
+#define EIR_FLAGS                   0x01  /* flags */
+#define EIR_UUID16_SOME             0x02  /* 16-bit UUID, more available */
+#define EIR_UUID16_ALL              0x03  /* 16-bit UUID, all listed */
+#define EIR_UUID32_SOME             0x04  /* 32-bit UUID, more available */
+#define EIR_UUID32_ALL              0x05  /* 32-bit UUID, all listed */
+#define EIR_UUID128_SOME            0x06  /* 128-bit UUID, more available */
+#define EIR_UUID128_ALL             0x07  /* 128-bit UUID, all listed */
+#define EIR_NAME_SHORT              0x08  /* shortened local name */
+#define EIR_NAME_COMPLETE           0x09  /* complete local name */
+#define EIR_TX_POWER                0x0A  /* transmit power level */
+#define EIR_DEVICE_ID               0x10  /* device ID */
+
 struct hci_request ble_hci_request(uint16_t ocf, int clen, void * status, void * cparam)
 {
 	struct hci_request rq;
